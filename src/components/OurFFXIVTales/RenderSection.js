573cc9436124eaf3d823298909894b09ef9930b6
David Singleton <davidjsingleton18@gmail.com>
2025-09-02 11:06:29 -0700

    Add start of FFXIV tales section creation via Javascript

---
+++ src/components/OurFFXIVTales/RenderSection.js
@@ -0,0 +1,9 @@
+function createTitle() {
+    const titleElement = document.createElement('h2');
+    titleElement.textContent = 'Random and Eris';
+    return titleElement;
+}
+
+export default function createFFXIVTalesSection() {
+    // filler
+}
