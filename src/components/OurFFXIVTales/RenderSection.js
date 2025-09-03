c3b35820a1266152600ffb50b701e13fbed0196f
David Singleton <davidjsingleton18@gmail.com>
2025-09-03 10:56:40 -0700

    Update name of our ffxiv tales creation file name to match its purpose
    Create our ffxiv tales section creation function

--- src/components/OurFFXIVTales/RenderSection.js
+++
@@ -1,9 +0,0 @@
-function createTitle() {
-    const titleElement = document.createElement('h2');
-    titleElement.textContent = 'Random and Eris';
-    return titleElement;
-}
-
-export default function createFFXIVTalesSection() {
-    // filler
-}
