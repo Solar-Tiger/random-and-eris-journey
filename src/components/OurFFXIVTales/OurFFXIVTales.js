c3c1c57b9ef32d9c089dca805bab982133aca53d
David Singleton <davidjsingleton18@gmail.com>
2025-09-05 17:45:02 -0700

    Add start of allowing our ffxiv tales section to switch between different displayed content

--- src/components/OurFFXIVTales/OurFFXIVTales.js
+++ src/components/OurFFXIVTales/OurFFXIVTales.js
@@ -5,16 +5,23 @@ import rengiaAndMiraImg1 from '../../assets/images/portrait_images/rengia-and-mi
 const ourFFXIVTalesContent = [
     {
         title: 'Random and Eris',
-        images: [randomAndErisImg1, randomAndErisImg1, rengiaAndMiraImg1],
+        images: [randomAndErisImg1, randomAndErisImg1, randomAndErisImg1],
         paragraph:
             "We get into all kinds of mischief on our adventure and enjoy recording the experience! Come check out all the images we've taken since October 2023!",
         linkText: 'Relive our tale',
+        link: 'pages/ffxivtales.html'
+    },
+    {
+        title: 'Rengia and Mira',
+        image: [rengiaAndMiraImg1, rengiaAndMiraImg1, rengiaAndMiraImg1],
+        paragraph:
+            'These two end up in Eorzea due to an unknown force and have more in common than they think, and you can experience their walk through Eorzea!',
         link: 'pages/ffxivtales.html'
     }
 ];
 
 // Function to export section
-export function createFFXIVTalesSection() {
+function createFFXIVTalesSection(currentIndex = 0) {
     const section = document.createElement('section');
     const title = document.createElement('h2');
     const imageContainer = document.createElement('div');
@@ -25,12 +32,12 @@ export function createFFXIVTalesSection() {
     section.classList.add('our-ffxiv-tales');
 
     // Create title
-    title.textContent = ourFFXIVTalesContent[0].title;
+    title.textContent = ourFFXIVTalesContent[currentIndex].title;
 
     // Create image container and images
     imageContainer.classList.add('image-container');
 
-    ourFFXIVTalesContent[0].images.forEach((img) => {
+    ourFFXIVTalesContent[currentIndex].images.forEach((img) => {
         const myImage = document.createElement('img');
 
         myImage.src = img;
@@ -39,14 +46,47 @@ export function createFFXIVTalesSection() {
     });
 
     // Create description
-    description.textContent = ourFFXIVTalesContent[0].paragraph;
+    description.textContent = ourFFXIVTalesContent[currentIndex].paragraph;
 
     // Create link
-    btnLink.textContent = ourFFXIVTalesContent[0].linkText;
-    btnLink.href = ourFFXIVTalesContent[0].link;
+    btnLink.textContent = ourFFXIVTalesContent[currentIndex].linkText;
+    btnLink.href = ourFFXIVTalesContent[currentIndex].link;
 
     // Append all elements
     section.append(title, imageContainer, description, btnLink);
 
     return section;
 }
+
+function changeDisplayedContent() {
+    let currentIndex = 0;
+    let slideLength = ourFFXIVTalesContent.length;
+
+    nextSlide(currentIndex, slideLength);
+    previousSlide(currentIndex, slideLength);
+}
+
+function nextSlide(slideIndex, totalSlideLength) {
+    const ourFFXIVTalesSection = document.querySelector('.our-ffxiv-tales');
+
+    ourFFXIVTalesSection.textContent = '';
+
+    let nextSlideIndex = (slideIndex + 1) % totalSlideLength;
+    createFFXIVTalesSection(nextSlideIndex);
+}
+
+function previousSlide(slideIndex, totalSlideLength) {
+    const ourFFXIVTalesSection = document.querySelector('.our-ffxiv-tales');
+
+    ourFFXIVTalesSection.textContent = '';
+
+    let previousSlideIndex =
+        (slideIndex - 1 + totalSlideLength) % totalSlideLength;
+    createFFXIVTalesSection(previousSlideIndex);
+}
+
+export {
+    ourFFXIVTalesContent,
+    createFFXIVTalesSection,
+    changeDisplayedContent
+};
