05b84cd0ba7c6c868133022af4a805c2f6e9fa11
David Singleton <davidjsingleton18@gmail.com>
2025-09-10 11:09:03 -0700

    Create function to create random and eris hero image

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,9 +1,50 @@
 import { ourFFXIVTalesContent } from './FFXIVTalesSection.js';
+import randomAndErisHeroImage from '../../assets/images/hero_images/random-and-eris-hugging-in-ffxiv.png';
 
 // FUNCTION TO CREATE HERO SECTION
 
 function createRandomAndErisHeroSection() {
-    const heroSection = 'My hero';
+    const randomAndErisHeroSection = document.createElement('section');
+    const randomAndErisHeroImg = document.createElement('img');
+    const randomAndErisHeroTextSection = document.createElement('section');
+    const randomAndErisHeroTitle = document.createElement('h1');
+    const randomAndErisHeroDescription = document.createElement('p');
+    const randomAndErisHeroLink = document.createElement('a');
+
+    // Add class to hero section
+    randomAndErisHeroSection.classList.add('random-and-eris-hero');
+
+    // Add hero image
+    randomAndErisHeroImg.src = randomAndErisHeroImage;
+
+    // Add class to text section of hero
+    randomAndErisHeroTextSection.classList.add('random-and-eris-hero-text');
+
+    // Add title to text section of hero
+    randomAndErisHeroTitle.textContent = 'Random and Eris Chaotic Journey';
+
+    // Add description text section of hero
+    randomAndErisHeroDescription.textContent =
+        "This website documents my best friend and I's chaotic journey of FFXIV, anime & Discord!";
+
+    // Add link to text section of hero
+    randomAndErisHeroLink.href = 'pages/ourstory.html';
+    randomAndErisHeroLink.textContent = 'Start the Journey Here';
+
+    // Append text content to Random and Eris text section of hero
+    randomAndErisHeroTextSection.append(
+        randomAndErisHeroTitle,
+        randomAndErisHeroDescription,
+        randomAndErisHeroLink
+    );
+
+    // Append every to Random and Eris hero section
+    randomAndErisHeroSection.append(
+        randomAndErisHeroImage,
+        randomAndErisHeroTextSection
+    );
+
+    return randomAndErisHeroSection;
 }
 
 // FUNCTION TO CREATE FFXIV TALES SECTION
@@ -58,4 +99,4 @@ function createFFXIVTalesSection(currentIndex = 0) {
     return ffxivTalesSection;
 }
 
-export { createFFXIVTalesSection };
+export { createRandomAndErisHeroSection, createFFXIVTalesSection };
