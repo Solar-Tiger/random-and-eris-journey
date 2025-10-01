480293a6cf3dd1f485a8d2ed9f992926fb75be72
David Singleton <davidjsingleton18@gmail.com>
2025-10-01 10:55:38 -0700

    Add image cards to second part of our story section

--- src/components/OurStory/OurStory.js
+++ src/components/OurStory/OurStory.js
@@ -1,8 +1,10 @@
+import randomAndEris from '../../assets/images/portrait_images/random-and-eris-tale-ffxiv.png';
+
 const imageCardInformation = [
-    { cardImage: 'Image', cardTitle: 'Title' },
-    { cardImage: 'Image', cardTitle: 'Title' },
-    { cardImage: 'Image', cardTitle: 'Title' },
-    { cardImage: 'Image', cardTitle: 'Title' }
+    { cardImage: randomAndEris, cardTitle: 'Random and Eris' },
+    { cardImage: randomAndEris, cardTitle: 'Random and Eris' },
+    { cardImage: randomAndEris, cardTitle: 'Random and Eris' },
+    { cardImage: randomAndEris, cardTitle: 'Random and Eris' }
 ];
 
 function createOurStoryReasonSection() {
@@ -22,6 +24,28 @@ function createOurStoryReasonSection() {
     const imageCardContainer = document.createElement('div');
 
     imageCardContainer.classList.add('image-card-container');
+
+    imageCardInformation.forEach((imageCard) => {
+        // Create Image Card figure
+        const ourStoryImageCardFigure = document.createElement('figure');
+
+        // Create image + figcaption
+        const ourStoryImage = document.createElement('img');
+
+        ourStoryImage.src = imageCard.cardImage;
+        ourStoryImage.style.width = '200px';
+
+        const ourStoryCaption = document.createElement('figcaption');
+
+        ourStoryCaption.textContent = imageCard.cardTitle;
+
+        ourStoryImageCardFigure.append(ourStoryImage, ourStoryCaption);
+
+        imageCardContainer.appendChild(ourStoryImageCardFigure);
+    });
+
+    // Append image cards to Our Story Section Container
+    ourStorySectionContainer.appendChild(imageCardContainer);
 
     return ourStorySectionContainer;
 }
