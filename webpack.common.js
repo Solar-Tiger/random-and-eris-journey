dc10f2fcdba52d4614eaabcd108a9c9035771da7
David Singleton <davidjsingleton18@gmail.com>
2025-12-03 11:05:26 -0800

    Fix path correction in dist folder

--- webpack.common.js
+++ webpack.common.js
@@ -51,7 +51,7 @@ export default {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'assets/images/[path][name][ext]'
+                    filename: '[path][name][ext]'
                 }
             },
             {
