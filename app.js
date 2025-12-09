6ba5957375dbd42838cc3a0ac36f99bc335ef2f3
Solar-Tiger <davidjsingleton30@yahoo.com>
2025-12-09 06:28:36 -0800

    Update recursive code to implement recursive file treversal

--- app.js
+++ app.js
@@ -6,7 +6,30 @@ fs.readdir('src/pages', (err, data) => {
         console.error(err);
         return;
     }
-    console.log(data);
+
+    for (const d of data) {
+        console.log(d);
+
+        fs.stat(`src/pages/${d}`, (err, stats) => {
+            if (err) {
+                console.error(err);
+            }
+
+            if (stats.isDirectory()) {
+                fs.readdir(`src/pages/${d}`, (err, files) => {
+                    if (err) {
+                        console.error(err);
+                    }
+
+                    for (const file of files) {
+                        console.log(file);
+                    }
+                });
+            }
+        });
+    }
+
+    // console.log(data);
 
     // for (const d in data) {
     //     console.log(data[0]);
