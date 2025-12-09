a21fcf800c1d575bec69aaa8a6396fa56cd29d6e
David Singleton <davidjsingleton18@gmail.com>
2025-12-09 10:55:09 -0800

    Add test traversal tree to main index.js for logging in the console

--- src/index.js
+++ src/index.js
@@ -30,3 +30,64 @@ nextBtn.addEventListener('click', () => {
 previousBtn.addEventListener('click', () => {
     changeContent.previousSlide();
 });
+
+const myObj = {
+    2023: {
+        casual: ['random-and-eris-1', 'random-and-eris-2', 'random-and-eris-3'],
+        seasonalEvents: {
+            valentioneDay: [
+                'random-and-eris-valentione-day-1',
+                'random-and-eris-valentione-day-2',
+                'random-and-eris-valentione-day-3'
+            ]
+        },
+        msq: {
+            aRealmReborn: [],
+            postARealmReborn: []
+        }
+    },
+
+    2024: {
+        casual: ['random-and-eris-1', 'random-and-eris-2', 'random-and-eris-3'],
+        seasonalEvents: {
+            valentioneDay: [
+                'random-and-eris-valentione-day-1',
+                'random-and-eris-valentione-day-2',
+                'random-and-eris-valentione-day-3'
+            ],
+            starlightCelebration: []
+        },
+        msq: [],
+        mounts: []
+    }
+};
+
+function listAllItems(objs) {
+    // Check if the argument is an object
+    if (objs === Object(objs) && !Array.isArray(objs)) {
+        // Log all keys and values of an object
+        const keyItem = Object.keys(objs);
+        const valueItem = Object.values(objs);
+
+        console.log(keyItem);
+
+        // Check if each key is an object and if so, repeat
+        for (const item in valueItem) {
+            // console.log(valueItem[item]);
+
+            listAllItems(valueItem[item]);
+        }
+
+        return;
+    } else if (Array.isArray(objs)) {
+        // If we reach an array, just log the items
+        for (const obj of objs) {
+            console.log(obj);
+        }
+    } else {
+        console.error('Not an object');
+        return;
+    }
+}
+
+listAllItems(myObj);
