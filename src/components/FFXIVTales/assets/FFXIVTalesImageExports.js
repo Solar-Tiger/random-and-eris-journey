f6d337bc850ca0e219be366bdbad17d4709bdefd
Solar_Tiger <davidjsingleton30@yahoo.com>
2026-01-07 14:37:18 -0800

    Move ffxiv tales page default image assets to proper location

---
+++ src/components/FFXIVTales/assets/FFXIVTalesImageExports.js
@@ -0,0 +1,13 @@
+import randomAndErisCasual01 from './images/random_and_eris_casual_xx23/random-and-eris-casual-01-xx23.PNG';
+import randomAndErisCasual02 from './images/random_and_eris_casual_xx23/random-and-eris-casual-02-xx23.PNG';
+import randomAndErisCasual03 from './images/random_and_eris_casual_xx23/random-and-eris-casual-03-xx23.PNG';
+import randomAndErisCasual04 from './images/random_and_eris_casual_xx23/random-and-eris-casual-04-xx23.PNG';
+import randomAndErisCasual05 from './images/random_and_eris_casual_xx23/random-and-eris-casual-05-xx23.PNG';
+
+export {
+    randomAndErisCasual01,
+    randomAndErisCasual02,
+    randomAndErisCasual03,
+    randomAndErisCasual04,
+    randomAndErisCasual05
+};
