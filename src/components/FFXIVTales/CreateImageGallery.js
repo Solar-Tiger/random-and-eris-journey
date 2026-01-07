1c2508dfdc80c856609a59bce98fea6112416b4f
Solar_Tiger <davidjsingleton30@yahoo.com>
2026-01-07 14:32:45 -0800

    Remove all unneeded code and finish function that creates default image gallery on ffxiv tales page

--- src/components/FFXIVTales/CreateImageGallery.js
+++ src/components/FFXIVTales/CreateImageGallery.js
@@ -1,55 +1,20 @@
-function fetchCloudinaryImage(publicId) {
-    const cloudName = 'duaozkbsv';
-
-    const url = `https://res.cloudinary.com/${cloudName}/image/upload/f_auto/q_auto/${publicId}`;
-
-    return url;
-}
-
-// random-and-eris-casual-4.png
-
-function getAllFilesInDirectory(directoryName, objs) {
-    // Check if the directory name equals the directory you're looking for AND it's an Object
-    if (directoryName !== objs.directory && objs === Object(objs)) {
-        for (let i = 0; i < objs.children.length; i++) {
-            getAllFilesInDirectory(directoryName, objs.children[i]);
-        }
-    }
-
-    // Make sure it's an Object else do nothing with it
-    else if (objs === Object(objs)) {
-        for (let i = 0; i < objs.children.length; i++) {
-            console.log(objs.children[i]);
-
-            const img = document.createElement('img');
-
-            img.src = fetchCloudinaryImage(objs.children[i]);
-
-            console.log(img);
-        }
-    }
-}
+import * as randomAndErisImages from './assets/FFXIVTalesImageExports.js';
 
 function createFFXIVTalesImageGallery() {
     const section = document.createElement('section');
 
-    for (let i = 1; i <= 5; i++) {
-        const testImg = document.createElement('img');
+    Object.values(randomAndErisImages).forEach((randomAndErisImage) => {
+        const img = document.createElement('img');
 
-        let myLink = fetchCloudinaryImage(
-            `random-and-eris-casual-0${i}-xx23.PNG`
-        );
+        img.src = randomAndErisImage;
 
-        testImg.src = myLink;
+        section.appendChild(img);
+    });
 
-        section.appendChild(testImg);
-    }
-
+    section.id = 'ffxiv-tales-image-gallery';
     section.classList.add('ffxiv-tales-image-gallery');
 
     return section;
 }
 
-// getAllFilesInDirectory('bad-guys', myObj);
-
-export { getAllFilesInDirectory, createFFXIVTalesImageGallery };
+export { createFFXIVTalesImageGallery };
