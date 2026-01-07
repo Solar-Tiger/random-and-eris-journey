87d94b35eaf41ed4d0b8dc21645e005879dd1d7b
Solar_Tiger <davidjsingleton30@yahoo.com>
2026-01-07 14:36:46 -0800

    Import proper sidebar for ffxiv tales page

--- src/pages/FFXIV_Tales/index.js
+++ src/pages/FFXIV_Tales/index.js
@@ -1,18 +1,15 @@
 import { createFFXIVTalesImageGallery } from '../../components/FFXIVTales/CreateImageGallery.js';
-import { updateImageGallery } from '../../components/FFXIVTales/UpdateImageGallery.js';
 import { createFFXIVTalesSidebar } from '../../components/FFXIVTales/FFXIVTalesAside.js';
 
 // updateImageGallery();
 
 const ffxivTalesSection = document.querySelector('.our-shared-adventures');
 
-// ffxivTalesSection.appendChild(createFFXIVTalesSidebar());
-
 let ffxivTalesImageGalleryContainer = document.querySelector(
     '.ffxiv-tales-image-gallery-container'
 );
 
 ffxivTalesImageGalleryContainer.append(
-    createFFXIVTalesSidebar(),
+    createFFXIVTalesSidebar,
     createFFXIVTalesImageGallery()
 );
